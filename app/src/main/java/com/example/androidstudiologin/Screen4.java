8cc071a43248adefee77ad47e7e81786879cf0f7
Isabel <isabelroldancordoba@hotmail.com>
2023-11-17 13:23:31 +0100

    Screen4 casi terminada

---
+++ app/src/main/java/com/example/androidstudiologin/Screen4.java
@@ -0,0 +1,47 @@
+package com.example.androidstudiologin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Screen4 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen4);
+
+
+        Spinner mySpinner = findViewById(R.id.menuDesplegable);
+
+        // Crear una lista de datos
+        List<String> listaDatos = new ArrayList<>();
+        listaDatos.add("Opción 1");
+        listaDatos.add("Opción 2");
+        listaDatos.add("Opción 3");
+
+        // Crear un ArrayAdapter y establecerlo en el Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listaDatos);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Establecer un tamaño de texto más grande para las opciones del Spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(adapter);
+    }
+
+    public void logout(View view){
+        //Intent i = new Intent(this, Screen1.class);
+
+        //startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/androidstudiologin/Screen4s.java
@@ -0,0 +1,14 @@
+package com.example.androidstudiologin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Screen4s extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen4s);
+    }
+}
