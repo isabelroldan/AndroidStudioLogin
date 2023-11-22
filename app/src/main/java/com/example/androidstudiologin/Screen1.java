04ceacfc1fa616aa9e33ecfad6c711dea7c63c80
Isabel <isabelroldancordoba@hotmail.com>
2023-11-22 22:56:38 +0100

    arreglo de errores

--- app/src/main/java/com/example/androidstudiologin/Screen1.java
+++ app/src/main/java/com/example/androidstudiologin/Screen1.java
@@ -2,14 +2,33 @@ package com.example.androidstudiologin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Locale;
 
 public class Screen1 extends AppCompatActivity {
 
     private EditText metName;
+    private EditText metPassword;
+
+
+
+    /*private SharedPreferences sharedPreferences;*/
+    private SharedPreferences sharedPreferences;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +36,137 @@ public class Screen1 extends AppCompatActivity {
         setContentView(R.layout.activity_screen1);
 
         metName = (EditText) findViewById(R.id.usuario);
+        metPassword = (EditText) findViewById(R.id.contraseña);
+
+        /*sharedPreferences =  PreferenceManager.getDefaultSharedPreferences(this);
+
+        ToggleButton toggleIdioma = findViewById(R.id.toggleIdioma);
+        toggleIdioma.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                cambiarIdioma("es");
+            } else {
+                cambiarIdioma("en");
+            }
+        });*/
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        ToggleButton toggleIdioma = findViewById(R.id.toggleIdioma);
+        toggleIdioma.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                cambiarIdioma("es");
+            } else {
+                cambiarIdioma("en");
+            }
+        });
     }
+
+
+    private void cambiarIdioma(String idioma) {
+        // Guardar el idioma seleccionado en SharedPreferences
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("idioma", idioma);
+        editor.apply();
+
+        // Obtener la configuración actual
+        Configuration config = getResources().getConfiguration();
+
+        // Establecer el nuevo idioma
+        Locale locale = new Locale(idioma);
+        Locale.setDefault(locale);
+        config.locale = locale;
+
+        // Actualizar la configuración de recursos
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+
+        // Actualizar las vistas manualmente (puedes poner todas las actualizaciones necesarias aquí)
+        actualizarVistas();
+    }
+
+    private void actualizarVistas() {
+        // Actualizar cada vista que pueda cambiar con el cambio de idioma
+        TextView textViewBienvenido = findViewById(R.id.textView);
+        textViewBienvenido.setText(R.string.bienvenido);
+
+        EditText editTextUsuario = findViewById(R.id.usuario);
+        editTextUsuario.setHint(R.string.usuario);
+
+        EditText editTextContraseña = findViewById(R.id.contraseña);
+        editTextContraseña.setHint(R.string.contraseña);
+
+        // Añade las siguientes líneas para los elementos adicionales
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setText(R.string.entrar);
+
+        TextView textViewContraseñaOlvidada = findViewById(R.id.textView2);
+        textViewContraseñaOlvidada.setText(R.string.contraseñaOlvidada);
+
+        TextView textViewNuevoUsuario = findViewById(R.id.textView3);
+        textViewNuevoUsuario.setText(R.string.nuevoUsuario);
+    }
+    /*private void cambiarIdioma(String idioma) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("idioma", idioma);
+        editor.apply();
+
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }*/
+
+    /*@Override
+    protected void attachBaseContext(Context newBase) {
+        // Cambiar el contexto de la aplicación para reflejar el nuevo idioma
+        String idioma = sharedPreferences.getString("idioma", "en");
+        Locale locale = new Locale(idioma);
+        Configuration config = new Configuration(newBase.getResources().getConfiguration());
+        Locale.setDefault(locale);
+        config.setLocale(locale);
+        super.attachBaseContext(newBase.createConfigurationContext(config));
+    }*/
 
     public void LogIn(View view){
-        Intent i = new Intent(this, Screen4.class);
 
-        i.putExtra("usuario", metName.getText().toString());
+        String usuario = metName.getText().toString();
 
-        startActivity(i);
+        String password = metPassword.getText().toString();
+
+        if(usuario.length()==0){
+            Toast.makeText(this, "Falta el usuario", Toast.LENGTH_SHORT).show();
+        }
+        if(usuario == null){
+            Toast.makeText(this, "Falta el usuario", Toast.LENGTH_SHORT).show();
+        }
+        if(password.length()==0){
+            Toast.makeText(this, "Falta la contraseña", Toast.LENGTH_SHORT).show();
+        }
+        if(password==null){
+            Toast.makeText(this, "Falta la contraseña", Toast.LENGTH_SHORT).show();
+        }
+        if(usuario.length()!=0 && password.length()!=0){
+            Toast.makeText(this, "Login correcto", Toast.LENGTH_SHORT).show();
+
+            Intent i = new Intent(this, Screen4.class);
+
+            i.putExtra("usuario", metName.getText().toString());
+
+            startActivity(i);
+        }
+        if(usuario=="usuario" && password=="contraseña"){
+            Toast.makeText(this, "Login correcto", Toast.LENGTH_SHORT).show();
+
+            Intent i = new Intent(this, Screen4.class);
+
+            i.putExtra("usuario", metName.getText().toString());
+
+            startActivity(i);
+        }
+
+
 
     }
 
-    public void newUser(View view){
+    /*public void newUser(View view){
         Intent i = new Intent(this, Screen2.class);
         startActivity(i);
     }
@@ -36,5 +174,5 @@ public class Screen1 extends AppCompatActivity {
     public void forgotPassword(View view){
         Intent i = new Intent(this, Screen3.class);
         startActivity(i);
-    }
+    }*/
 }
