3708ea1e2ae1daa0761f9544feb6a978ae6b650b
martaalopez <119664444+martaalopez@users.noreply.github.com>
2023-11-23 20:15:22 +0100

    layout

--- app/src/main/java/com/example/androidstudiologin/Screen2.java
+++ app/src/main/java/com/example/androidstudiologin/Screen2.java
@@ -24,7 +24,7 @@ public class Screen2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_screen2);
 
         emailEditText = findViewById(R.id.comp_text9);
         userEditText = findViewById(R.id.comp_text2);
